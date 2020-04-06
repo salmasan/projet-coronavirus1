6427ef205d68d10675be578afc75c3e2085fab1c
salma <salma.sanoussi96@gmail.com>
2020-04-06 01:50:48 +0100

    navbar

--- Starter-file/gulpfile.js
+++ Starter-file/gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('browserSync', function() {
 
 // Compilation SASS 
 gulp.task('sass', function() {
-    return gulp.src('app/css/*.scss') // Gets all files ending with .scss in app/scss
+    return gulp.src('app/scss/*.scss') // Gets all files ending with .scss in app/scss
       .pipe(sass())
       .pipe(gulp.dest('app/css'))
      
